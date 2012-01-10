3c9e14162b03cea58784d12df116d742978e93df
debmint <debmint@users.noreply.github.com>
2012-01-10 14:09:56 +0000

    Committing this start so development can be further made on other computer

--- gtktblprintpg.c
+++ gtktblprintpg.c
@@ -1,5 +1,15 @@
 /* ************************************************************************ *
  * gtktblprintpg.c - Mainline module for GtkTblPrintPg library function     $
+ *                                                                          $
  * $Id::                                                                    $
  * ************************************************************************ */
 
+#include "gtktblprintpg.h"
+
+/* **************************************************************** *
+ * render_document() - First-level routine for rendering document   *
+ * **************************************************************** */
+
+void render_document(PGresult *data)
+{
+}
--- gtktblprintpg.h
+++ gtktblprintpg.h
@@ -3,3 +3,36 @@
  * $Id::                                                                    $
  * ************************************************************************ */
 
+#include <gtk/gtk.h>
+#include <libpq-fe.h>
+
+#define DFLTFONT "Sans Serif"
+#define DFLTSIZE 10
+
+typedef struct pg_mar {
+    double left,
+           top,
+           right,
+           bottom
+} PGMAR *PPGMAR;
+
+// This structure contains all that is needed to insert a single cell
+typedef struct cell_info {
+    double x,               // X position for the current insert
+           y;               // Y position for the current insert
+    double cellwidth;
+    char  *celltext;        // Text to insert into the cell
+} CELLINF, PCELLINF;
+
+typedef struct cell_props {
+    double pgwidth,
+           pgheight;
+    PGMAR  margins;
+    char  *fontname;
+    double fontsize;
+    char  *txtalign;
+} CELLPROPS, *PCELLPROPS
+
+typedef struct tbl_data {
+} TBLDATA, *PTBLDATA;
+
