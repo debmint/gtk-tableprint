0b9d74ec4ea733994aef533925e24bfb3dc5a884
debmint <debmint@users.noreply.github.com>
2012-01-13 15:05:24 +0000

    Committing PARTIAL additions.  These consist of some structure definitions that may be useful, etc.  The purpose for this commit is to share them in order to attempt to use the Glib XML parser to read the formatting.

--- gtktblprintpg.c
+++ gtktblprintpg.c
@@ -1,15 +1,70 @@
 /* ************************************************************************ *
  * gtktblprintpg.c - Mainline module for GtkTblPrintPg library function     $
  *                                                                          $
+ * The setup of the control for the print output is contained in a          $
+ * GHashTable structure.  This contains headers for each part of the        $
+ * document.  The divisions are:                                            $
+ * "docheader" - the line printed at the begin of the document              $
+ * "pageheader" - the header for each page                                  $
+ * "groups" - The data to be grouped                                        $
+ * "defaultcell" - The default font specifications for cells which are not  $
+ *      specifically defined.                                               $
+ * Each of the above (GHashTable's), can have the following divisions       $
+ * "cols" - A GPtrArray defining the column specifications for each column. $
+ *      This can have the following defs.                                   $
+ *      "colwidth" (mandatory) - the percentage of the page width for this  $
+ *              column.                                                     $
+ *      "font" - a GHashTable with specs for "family", "size", "alignment", $
+ *          and "weight". These will be converted to a PangoFontDescription $
+ *                                                                          $
  * $Id::                                                                    $
  * ************************************************************************ */
 
+#include <stdlib.h>
 #include "gtktblprintpg.h"
 
-/* **************************************************************** *
- * render_document() - First-level routine for rendering document   *
- * **************************************************************** */
+/* ************************************************************************ *
+ 
+ * ************************************************************************ */
 
-void render_document(PGresult *data)
+static void setup_formatting(GHashTable *);
+
+gboolean Formatted = FALSE;
+GPtrArray *dochead;
+FONTINF defaultfont = {"Serif", 10, PANGO_STYLE_NORMAL, PANGO_WEIGHT_NORMAL};
+
+CELLINF defaultcell =
 {
+    0, 0, 0,            // x, y position, and cell width not defined
+    &defaultfont,       // font
+    NULL,               // PangoFont
+    0,                  // txtsource
+    NULL,               // celltext
+    PANGO_ALIGN_LEFT    // txtalign
+};
+
+struct xml_udat {
+    int depth;
+    PGRPINF parent;
+    PGRPINF curfmt;
+} XMLData;
+
+PGRPINF FmtList;    // The formatting tree;
+
+/* ******************************************************************** *
+ * render_document() - Entry routine for rendering document             *
+ * Passed:  (1) data : a PGresult ordered in the way it is to appear    *
+ *                  in the printout.                                    *
+ *          (2) formatting : A GHashTable containing all the formatting *
+ *                  data.                                               *
+ * ******************************************************************** */
+
+void render_document(PGresult *data, GHashTable *formatting)
+{
+    defaultfont.pangofont = pango_font_description_new_from_string(
+                "Serif PANGO_STYLE_NORMAL 10");
+    pango_font_description_set_weight(defaultfont.pangofont,
+            PANGO_WEIGHT_NORMAL);
+    setup_formatting(formatting);
 }
+
--- gtktblprintpg.h
+++ gtktblprintpg.h
@@ -4,35 +4,64 @@
  * ************************************************************************ */
 
 #include <gtk/gtk.h>
+#include <glib.h>
 #include <libpq-fe.h>
 
 #define DFLTFONT "Sans Serif"
 #define DFLTSIZE 10
 
+// enumerations for data source for text
+enum {
+    TSRC_STATIC = 1,    // Constant text
+    TSRC_DATA,          // Table column
+    TSRC_NOW,           // Current time
+    TSRC_PRINTF         // Printf-formatted data
+};
+
 typedef struct pg_mar {
     double left,
            top,
            right,
-           bottom
-} PGMAR *PPGMAR;
+           bottom;
+} PGMAR, *PPGMAR;
+
+typedef struct font_inf {
+    char *family;
+    int   size;
+    PangoStyle  style;
+    PangoWeight weight;
+} FONTINF, *PFONTINF;
 
 // This structure contains all that is needed to insert a single cell
 typedef struct cell_info {
     double x,               // X position for the current insert
-           y;               // Y position for the current insert
+           y;               // Y position for the current insert (not used???)
     double cellwidth;
+    PFONTINF font;          // Used to pass to library, library sets up
+                            // pangofont from this
+    PangoFontDescription *pangofont;    // Derived from "font" pointer
+    int   txtsource;        // Where to get source
     char  *celltext;        // Text to insert into the cell
-} CELLINF, PCELLINF;
+    int    txtjust;         // Justification - right/left/center
+} CELLINF, *PCELLINF;
+
+//typedef struct line_props {
+//} LINEPROP, *PLINEPROP;
+
+typedef struct grp_info {
+    int colcount;
+    GRPTY grptype;                  // Whether it's group, body, etc
+    struct grp_info *grpparent;
+    struct grp_info *grpchild;      // NULL if it's body
+    CELLINF **celldefs;             // Pointer to CELLINF array
+} GRPINF, *PGRPINF;
 
 typedef struct cell_props {
-    double pgwidth,
-           pgheight;
-    PGMAR  margins;
-    char  *fontname;
-    double fontsize;
-    char  *txtalign;
-} CELLPROPS, *PCELLPROPS
+    double   pgwidth,
+             pgheight;
+    PGMAR    margins;
+} CELLPROPS, *PCELLPROPS;
 
-typedef struct tbl_data {
-} TBLDATA, *PTBLDATA;
+//typedef struct tbl_data {
+//} TBLDATA, *PTBLDATA;
 
