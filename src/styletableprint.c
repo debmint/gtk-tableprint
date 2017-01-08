b10d0d8faab5d1e4eb8eef6646a7e1383cfa7a8a
debmint <debmint@users.noreply.github.com>
2017-01-07 19:39:40 -0600

    Added copyright notices into source files as well as adding files COPYING and README to repository.

--- src/styletableprint.c
+++ src/styletableprint.c
@@ -1,3 +1,23 @@
+/*
+Copyright (c) 2017 David Breeding
+
+This file is part of tableprint.
+
+tableprint is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+tableprint is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with tableprint (see the file "COPYING").  If not,
+see <http://www.gnu.org/licenses/>.
+*/
+
 /* ************************************************************************ *
  * styletableprint.c - Mainline module for TablePrint library function      $
  *                                                                          $
